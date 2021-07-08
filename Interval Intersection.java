4500937304d39fbafb53b7fb005ad5ba638f5ff2
Shree Devi <72245796+sreedevi-i@users.noreply.github.com>
2021-07-08 14:41:27 +0530

    Update Interval Intersection.java

--- Interval Intersection.java
+++ Interval Intersection.java
@@ -1,3 +1,27 @@
+/*
+Given a two-dimensional integer list intervals of the form [start, end] representing intervals (inclusive), return their intersection, that is, the interval that lies within all of the given intervals.
+
+You can assume that the intersection will be non-empty.
+
+Constraints
+
+1 ≤ n ≤ 100,000 where n is the length of intervals
+
+Example 1
+
+Input
+intervals = [
+    [1, 100],
+    [10, 50],
+    [15, 65]
+]
+
+Output
+[15, 50]
+Explanation
+Consider the ranges [1, 100], [10, 50], [15, 65] on a line. The range [15, 50] is the only interval that is included by all of them.
+*/
+
 import java.util.*;
 
 class Solution {
