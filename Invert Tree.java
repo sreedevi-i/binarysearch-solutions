e8fadce69fe68cfd6d9ccb4a77b92a9b53ef80b9
Shree Devi <72245796+sreedevi-i@users.noreply.github.com>
2021-07-10 19:35:44 +0530

    Create Invert Tree.java

---
+++ Invert Tree.java
@@ -0,0 +1,23 @@
+/* Given a binary tree root, invert it so that its left subtree and right subtree are swapped and the children are recursively inverted. */
+import java.util.*;
+
+/**
+ * public class Tree {
+ *   int val;
+ *   Tree left;
+ *   Tree right;
+ * }
+ */
+class Solution {
+    public Tree solve(Tree root) {
+        if(root==null)
+        return root;
+        Tree  node=root.left;
+        root.left=root.right;
+        root.right=node;
+        solve(root.left);
+        solve(root.right);
+        return root;
+        
+    }
+}
