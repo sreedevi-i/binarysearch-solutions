3e53a45e7a207eff34a65f3916a751c8dfeb252b
Shree Devi <72245796+sreedevi-i@users.noreply.github.com>
2021-07-29 23:43:42 +0530

    Create Remove Last Duplicate Entries.java

---
+++ Remove Last Duplicate Entries.java
@@ -0,0 +1,26 @@
+/* Given a list of integers nums, find all duplicate numbers and delete their last occurrences.*/
+import java.util.*;
+
+class Solution {
+    public int[] solve(int[] nums) {
+        
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            if (map.containsKey(nums[i]))
+                map.put(nums[i], i);
+            else
+                map.put(nums[i], -1);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (map.get(nums[i]) != i)
+                list.add(nums[i]);
+        }
+
+        int m = list.size();
+        int res[] = new int[m];
+        for (int i = 0; i < m; i++) res[i] = list.get(i);
+        return res;
+    }
+}
