3e6ff3a28f5728595ab8ef36d1913dfb92fa6cfd
Shree Devi <72245796+sreedevi-i@users.noreply.github.com>
2021-08-03 20:29:06 +0530

    Update Shortest String.java

--- Shortest String.java
+++ Shortest String.java
@@ -1,3 +1,6 @@
+/* Given a string s consisting only of "1"s and "0"s, you can delete any two adjacent letters if they are different.
+
+Return the length of the smallest string that you can make if you're able to perform this operation as many times as you want.*/
 import java.util.*;
 
 class Solution {
