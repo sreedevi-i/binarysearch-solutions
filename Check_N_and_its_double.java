6b70373836bd1388a0db9a4191376f6135a6a78b
Sreedevi Itta <72245796+sreedevi-i@users.noreply.github.com>
2024-12-01 17:38:31 -0600

    Update Check_N_and_its_double.java

--- Check_N_and_its_double.java
+++ Check_N_and_its_double.java
@@ -1,8 +1,8 @@
-//* 1346. Check If N and Its Double Exist: Given an array arr of integers, check if there exist two indices i and j such that :
+// 1346. Check If N and Its Double Exist: Given an array arr of integers, check if there exist two indices i and j such that :
 
-i != j
-0 <= i, j < arr.length
-arr[i] == 2 * arr[j] *//
+// i != j
+// 0 <= i, j < arr.length
+// arr[i] == 2 * arr[j] 
 
   class Solution {
     public boolean checkIfExist(int[] arr) {
